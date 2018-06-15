af1e97bb283974354d1d6418f9f9eb3600684d7e
Yann Herklotz <ymherklotz@gmail.com>
2018-06-15 20:09:32 +0100

    Fixing emacsclient $EDITOR and $VISUAL

--- zsh/.zsh/export.zsh
+++ zsh/.zsh/export.zsh
@@ -1,7 +1,7 @@
 export GITHUB_HOME=$HOME/Github
 export ALTERNATE_EDITOR=""
-export EDITOR='emacsclient -nw -a " "'
-export VISUAL='emacsclient -c -a " "'
+export EDITOR='emacsclient -nw -a ""'
+export VISUAL='emacsclient -c -a ""'
 
 export PATH="{$PATH}:${HOME}/.gem/ruby/2.5.0/bin:/usr/local/bin"
 export PATH="{$PATH}:${HOME}/.local/bin"
