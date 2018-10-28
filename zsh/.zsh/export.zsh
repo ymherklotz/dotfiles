9ecfa7248814e8d7aa9d1976474a5a0ebd4a3c7a
Yann Herklotz <ymherklotz@gmail.com>
2018-10-28 14:11:57 +0000

    Add better default for emacs opening

--- zsh/.zsh/export.zsh
+++ zsh/.zsh/export.zsh
@@ -1,7 +1,7 @@
 export GITHUB_HOME=$HOME/Github
 export ALTERNATE_EDITOR=""
-export EDITOR='emacsclient -nw -a ""'
-export VISUAL='emacsclient -c -a ""'
+export EDITOR='emacsclient -nw -a "emacs -Q -nw"'
+export VISUAL='emacsclient -c -a "emacs -Q"'
 
 export PATH="${HOME}/.gem/ruby/2.5.0/bin:/usr/local/bin:${PATH}"
 export PATH="${HOME}/.local/bin:${PATH}"
