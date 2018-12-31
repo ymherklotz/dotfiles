bfa90c75f8c8dea9a3e17f8d65bb82c02fb84e9c
Yann Herklotz <ymherklotz@gmail.com>
2018-12-31 10:18:32 +0100

    Add 256colour to xterm, add password store export

--- zsh/.zsh/export.zsh
+++ zsh/.zsh/export.zsh
@@ -1,7 +1,8 @@
 export GITHUB_HOME=$HOME/Github
-export ALTERNATE_EDITOR=""
-export EDITOR='emacsclient -nw -a "emacs -Q -nw"'
-export VISUAL='emacsclient -c -a "emacs -Q"'
+export ALTERNATE_EDITOR="emacs -nw -Q"
+export EDITOR='emacsclient -nw'
+export VISUAL='emacsclient -c'
+export TERM='xterm-256color'
 
 export PATH="${HOME}/.gem/ruby/2.5.0/bin:/usr/local/bin:${PATH}"
 export PATH="${HOME}/.local/bin:${PATH}"
@@ -9,3 +10,5 @@ export PATH="/usr/bin:${PATH}"
 export PATH="${HOME}/.yarn/bin:${PATH}"
 
 export CDPATH='.:..:../..'
+
+export PASSWORD_STORE_GPG_OPTS="--passphrase-file ${HOME}/.config/.yann_gpg"
