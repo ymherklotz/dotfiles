df7d27e6604587caa9adaa89f8ff378d2e359695
Yann Herklotz <ymherklotz@gmail.com>
2019-01-28 12:39:00 +0000

    Update ruby path and remove CD_PATH

--- zsh/.zsh/export.zsh
+++ zsh/.zsh/export.zsh
@@ -4,11 +4,9 @@ export EDITOR='emacsclient -nw'
 export VISUAL='emacsclient -c'
 export TERM='xterm-256color'
 
-export PATH="${HOME}/.gem/ruby/2.5.0/bin:/usr/local/bin:${PATH}"
+export PATH="${HOME}/.gem/ruby/2.6.0/bin:/usr/local/bin:${PATH}"
 export PATH="${HOME}/.local/bin:${PATH}"
 export PATH="/usr/bin:${PATH}"
 export PATH="${HOME}/.yarn/bin:${PATH}"
-
-export CDPATH='.:..:../..'
 
 export PASSWORD_STORE_GPG_OPTS="--passphrase-file ${HOME}/.config/.yann_gpg"
