25f006c50428c0727a3f59693d4b3185327ece3c
Yann Herklotz Grave <git@yannherklotzgrave.com>
2019-02-25 18:13:39 +0000

    Add org clock to mode line

--- emacs/init.el
+++ emacs/init.el
@@ -1,2 +1,19 @@
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
+
+(require 'package)
+
+(defvar gnu          '("gnu"          . "https://elpa.gnu.org/packages/"))
+(defvar melpa        '("melpa"        . "https://melpa.org/packages/"))
+(defvar melpa-stable '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(defvar org-elpa     '("org"          . "https://orgmode.org/elpa/"))
+
+(setq package-archives nil)
+(add-to-list 'package-archives melpa-stable t)
+(add-to-list 'package-archives melpa t)
+(add-to-list 'package-archives gnu t)
+(add-to-list 'package-archives org-elpa t)
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
 (org-babel-load-file (expand-file-name "loader.org" init-dir))
