8d69c2537adc4500e1d15d76a9c5d5c0be47927b
Yann Herklotz <git@yannherklotz.com>
2019-07-25 13:37:02 +0200

    Update environment variables and add direnv and nix support

---
+++ zsh/.zsh/startup.zsh
@@ -0,0 +1,10 @@
+# Nix
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+
+# direnv
+
+eval "$(direnv hook zsh)"
+
+# End Nix
