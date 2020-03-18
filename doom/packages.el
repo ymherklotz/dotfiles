208df59a0a4545c2da0ac75a2cb513ebbd391b86
Yann Herklotz <git@yannherklotz.com>
2020-03-18 17:39:19 +0000

    Disable undo-tree

--- doom/packages.el
+++ doom/packages.el
@@ -50,6 +50,9 @@
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
 
+;; Disabling packages
+(package! undo-tree :disable t)
+
 ;; Org mode dependencies
 (package! org-ref)
 
