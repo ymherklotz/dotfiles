9dc594db45db34554d98d16d8a704ecb11cccd93
Yann Herklotz <git@yannherklotz.com>
2022-03-31 13:47:38 +0100

    Add org mode configuration file

--- doom/init.el
+++ doom/init.el
@@ -176,7 +176,7 @@
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens)
 
        :os
